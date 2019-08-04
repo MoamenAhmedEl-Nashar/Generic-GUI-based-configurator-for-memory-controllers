3d688f4e036bad56f765507da7ec80e4c8159ef8
unknown <moamen.ahmed1@hotmail.com>
2019-08-04 02:19:07 +0200

    parameters with split statements

---
+++ main.py
@@ -0,0 +1,130 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter.ttk as ttk
+from lark import Lark
+from scrframe import *
+
+class Root(Tk):
+
+    # class attributes (same for all instances of a class)
+    # put any regex in / /  
+    parameter_declaration = Lark("""
+    start:"parameter" WORD "=" NUMBER  
+    WORD:/ [a-zA-Z][a-zA-Z_0-9]* /
+    COMMENT: ("/*" /.*/ "*/") | "//" / .* /
+    IGNORED: " " | NEWLINE | COMMENT
+    %import common.NUMBER
+    %import common.NEWLINE
+    %ignore IGNORED
+    """
+    )
+    # instance attributes (different for every instance of a class.)
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.parameters = {}
+        self.entries = []
+        self.edit_param = []
+        self.file_path = ""
+
+        self.style = ttk.Style()
+        self.style.configure("TFrame",
+                        padding=9,
+                        relief="flat",
+                        bg="LightBlue4",
+                        fg="gray15")
+        self.style.configure("TButton", padding=9, relief="raised", font=("Helvetica ", 20))
+        self.title('Generic GUI-based configurator')
+        #w, h = self.winfo_screenwidth(), self.winfo_screenheight()
+        #self.geometry("%dx%d+0+0" % (w, h))
+
+        self.frame_left = VerticalScrolledFrame(self, borderwidth=4)
+        self.frame_right = ttk.Frame(self, borderwidth=4)
+        self.frame_bottom = ttk.Frame(self, borderwidth=4)
+
+        self.upload_button = ttk.Button(
+        	self.frame_bottom,
+        	text="upload",
+        	command=lambda: self.read_file()
+        )
+        self.save_button = ttk.Button(
+        	self.frame_bottom,
+        	text="save",
+        	command=lambda: self.save_file()
+        )
+
+        ### grid ###
+        self.frame_left.grid(row=0, column=0, sticky="nsew")
+        self.frame_right.grid(row=0, column=1, sticky="nsew")
+        self.frame_bottom.grid(row=1, column=0, sticky="nsew")
+
+        ## frame bottom
+        self.upload_button.grid(row=0, column=0, sticky="nsew")
+        self.save_button.grid(row=0, column=1, sticky="nsew")
+
+    # methods
+    def read_file(self):
+    
+        self.file_path = filedialog.askopenfilename()
+        
+        self.file_path = self.file_path
+        # input sv file reading
+        with open(self.file_path, "r") as input_file:
+            self.source_code = input_file.read()
+
+        # search input code to get self.parameters 
+        st_index = 0
+        self.edit_param = []
+        for statement in self.source_code.split(";"):
+            try:
+                token = self.parameter_declaration.parse(statement)
+                token_word = token.children[0]
+                token_number  = token.children[1]
+                name = token_word[0:]
+                value = token_number[0:]
+                self.parameters[name] = value
+                self.edit_param.append(st_index)
+                st_index += 1
+                print(name, value)
+            except:
+                st_index += 1
+                continue
+        
+        self.edit_param = self.edit_param
+        print(self.edit_param)
+        # given a list of self.parameters, build the frame_left self.parameters input with scrollbar
+        r=0
+        
+        for name, value in self.parameters.items():
+            param_label = ttk.Label(self.frame_left.interior, text=name, font=("Courier", 20))
+            param_entry = ttk.Entry(self.frame_left.interior)
+            param_entry.insert(END, value)
+           
+            self.entries.append(param_entry)
+            param_label.grid(row=r, column=0)
+            param_entry.grid(row=r, column=1)
+            r+=1
+        
+    def save_file(self):
+        i = 0
+        for name in self.parameters.keys():
+            self.parameters[name] = self.entries[i].get()
+            i+=1
+        ## modify input file
+        input_file = open(self.file_path, "w") 
+        statements = self.source_code.split(";")
+        
+        for i in range(len(self.edit_param)):
+            statement = statements[self.edit_param[i]]
+            token = self.parameter_declaration.parse(statement)
+            token_word = token.children[0]
+            token_number  = token.children[1]
+            name = token_word[0:]
+            value = token_number[0:]
+            new_statement = statement.replace(value, self.parameters[name])
+            statements[self.edit_param[i]] = new_statement
+        new_code = ";".join(statements)
+        input_file.write(new_code)
+        input_file.close()
+        
+root = Root()
+root.mainloop()
---
+++ test_random_choose.v
@@ -0,0 +1,128 @@
+`timescale 1ns/1ns
+
+
+/*
+test bench for testing random choose module
+*/
+module test ;
+/*j*/ 
+ parameter WIDTH1 = 10; /*h*/
+ /*h*/ 
+ parameter WIDTH2 = 2;
+ parameter WIDTH3 = 31; // modify if ...
+ parameter WIDTH4 = 31;
+ // modify if ...
+ parameter WIDTH5 = 31;
+ parameter WID = 100;
+ parameter WIDTH6 = 31;
+ parameter WIDTH7 = 31;
+ parameter WIDTH8 = 31;
+ parameter WIDTH9 = 31;
+ parameter WIDTH10 = 31;
+ parameter WIDTH11= 31;
+ parameter WIDTH12= 31;
+ parameter WIDTH13= 31;
+ parameter WIDTH14= 31;
+ parameter WIDTH15= 31;
+ parameter WIDTH16= 31;
+ parameter WIDTH17= 31;
+ parameter WIDTH18= 31;
+ parameter WIDTH19 = 31;
+ parameter WIDTH20 = 31;
+ parameter WIDTH21 = 31;
+ parameter WIDTH22 = 31;
+ parameter WIDTH23 = 31;
+
+ // Inputs
+ reg clock;
+ reg reset;
+ reg enable;
+ reg [WIDTH:0]weight0,weight1,weight2,weight3;
+ reg [WIDTH:0]seed;
+ 
+ // Outputs
+ wire  [1:0] out_segment_number;
+ 
+ // Instantiate the Unit Under Test (UUT)
+ RandomChoose  #(.WIDTH(WIDTH))uut (
+  .in_clock(clock), 
+  .in_reset(reset),
+  .in_enable(enable),
+  .in_weight0(weight0),
+  .in_weight1(weight1),
+  .in_weight2(weight2),
+  .in_weight3(weight3),
+  .in_seed(seed), 
+  .out_segment_number(out_segment_number)
+ );
+  
+ initial begin
+  clock = 0;
+  forever
+   #50 clock = ~clock;
+  end
+   
+ initial begin
+  // Initialize Inputs
+  ////////////////
+  weight0 = 2;
+  weight1 = 4;
+  weight2 = 2;
+  weight3 = 0;
+  ///////////////
+  reset = 0;
+  enable = 0;
+  // Wait 100 ns for global reset to finish
+  #100;
+      seed = 1;
+      reset = 1;
+      enable = 1;
+  #200;
+  reset = 0;
+  #100
+  enable=0;
+  #200
+  enable=1;
+  #500
+  enable=0;
+  #300
+  enable=1;
+ 
+  
+ end
+  
+ initial begin
+ $display("out_segment_number");
+ //$monitor("%d", out_segment_number);
+ end     
+
+
+integer count0=0;
+integer count1=0;
+integer count2=0;
+integer count3=0;
+ always @(posedge clock)
+ begin
+    $monitor("count of segment 0 is %d",count0);
+    $monitor("count of segment 1 is %d",count1);
+    $monitor("count of segment 2 is %d",count2);
+    $monitor("count of segment 3 is %d",count3);
+
+   if(out_segment_number==0)
+   begin
+     count0=count0+1;
+   end
+   if(out_segment_number==1)
+   begin
+     count1=count1+1;
+   end
+   if(out_segment_number==2)
+   begin
+     count2=count2+1;
+   end
+   if(out_segment_number==3)
+   begin
+     count3=count3+1;
+   end
+ end
+endmodule
