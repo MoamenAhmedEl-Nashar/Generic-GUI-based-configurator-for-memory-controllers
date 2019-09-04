584e81ae90240f2818d89bc28a091a7f5ef44037
unknown <moamen.ahmed1@hotmail.com>
2019-09-04 14:43:52 +0200

    add scenarios comments

--- main.py
+++ main.py
@@ -3,6 +3,55 @@ in test benches easily, it parses the verilog input file, than extract
 parameters for each module instance. Also, it parse `include and `define
 to allow the user to control `ifdef that controls module instances or
 controls code within them.
+##########################    Scenarios     ##############################
+If you choose design file option, then click upload:
+•	Click upload
+•	Choose file with any extension
+•	The tool searches input code to get `ifdefs
+•	The tool saves all identifiers that comes after `ifdefs and puts them in defines dictionary.
+•	The tool searches for `defines and modify the defines dictionary with 1 for all defined identifiers.
+•	The tool shows all `ifdef identifiers with check buttons on the window
+•	If there are nothing to show, the tool says “no defines found”
+    If you click save:
+    	The tool saves the defines values from the user and updates the defines dictionary
+    	The tool opens the input file and modifies defines only. 
+If you click arrow to show parameters:
+o	The tool modifies parsers to ignore undefined sections (`ifdef “not defined identifier”)
+o	The tool parses the input file to get any parameter declaration
+o	The tool shows all parameters on the window
+o	If there are nothing to show, the tool says “There are no parameters”
+    If you click save:
+    	The tool saves the defines values from the user and updates the defines dictionary.
+    	The tool saves the parameters values from the user and updates the parameters dictionary.
+    	The tool opens the input file and modifies it. 
+
+
+If you choose test file option, then click upload:
+•	Click upload
+•	Choose file with any extension
+•	The tool searches input code to get `ifdefs
+•	The tool saves all identifiers that comes after `ifdefs and puts them in defines dictionary.
+•	The tool searches for `defines and modify the defines dictionary with 1 for all defined identifiers.
+•	The tool shows all `ifdef identifiers with check buttons on the window
+•	If there are nothing to show, the tool says “no defines found”
+    If you click save:
+    	The tool saves the defines values from the user and updates the defines dictionary
+    	The tool opens the input file and modifies defines only. 
+If you click arrow to show parameters:
+o	The tool modifies parsers to ignore undefined sections (`ifdef “not defined identifier”)
+o	The tool parses the input file to get any parameter declaration
+o	The tool parses the input file to get one module instance
+o	The tool saves the module name and module parameter names
+o	The tool shows “module: “module name
+o	The tool parser the included file (with extension “.v”)
+o	The tool shows module parameters names (from the input file and the included file) on the window
+o	If there are nothing to show, the tool says “There are no parameters”
+    If you click save:
+    	The tool saves the defines values from the user and updates the defines dictionary.
+    	The tool saves the parameters values from the user and updates the parameters dictionary.
+    	The tool opens the input file and modifies it. 
+
+
 """
 from tkinter import *  # the GUI is based on Tkinter
 from tkinter import filedialog  # filedialog is to allow uploading any file
